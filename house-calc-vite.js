01a724d111a3df417c011ee770aa93aa845a3131
Dang Thanh Nguyen <Dang.ThanhNguyen@premex.com>
2026-02-21 20:10:08 +0700

    remove unused item

--- house-calc-vite.js
+++ house-calc-vite.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/house-calculator/', // Đổi thành tên repo của bạn trên GitHub
+  base: '/house-calculator/', 
 })
